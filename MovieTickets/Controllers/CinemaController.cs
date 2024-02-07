8504a6a64fb1fbb1d649abfceb02b24d9cc2ddae
yosefha4 <yosefha4@ac.sce.ac.il>
2024-02-07 16:31:46 +0200

    Create Cinema controller & view

---
+++ MovieTickets/Controllers/CinemaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+
+namespace MovieTickets.Controllers
+{
+    public class CinemaController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+
+        public CinemaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View(allCinemas);
+        }
+    }
+}
