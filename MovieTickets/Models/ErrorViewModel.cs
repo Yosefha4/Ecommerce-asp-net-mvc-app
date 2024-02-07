1943292761e9445a75e87898c63862f1bd21c9b3
yosefha4 <yosefha4@ac.sce.ac.il>
2024-02-07 13:40:52 +0200

    Initial project

---
+++ MovieTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
