2b25deb1e7c61766985025672b8bb13eb9c93eeb
yosefha4 <yosefha4@ac.sce.ac.il>
2024-02-07 16:30:48 +0200

    Create Actor controller & view

---
+++ MovieTickets/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Data;
+
+namespace MovieTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
