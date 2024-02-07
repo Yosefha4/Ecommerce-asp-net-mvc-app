ef5cefec7fbfdc1141f638cb672aff89e1c30ac5
yosefha4 <yosefha4@ac.sce.ac.il>
2024-02-07 16:32:57 +0200

    Create Movies controller

---
+++ MovieTickets/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+
+namespace MovieTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;   
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var allMovies = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
